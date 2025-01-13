bb878ca8846aa0edb4e3160e9f40b1378546171c
Riccardo Spadon <spadon.riccardo@gmail.com>
2025-01-13 17:29:56 +0100

    Spotify-Clone
    - Modificate le card in base alla ricerca che si fa (artista, canzone o album), ognuna è personalizzata;
    - Ritocco di qualche elemento css;

--- script.js
+++ script.js
@@ -13,61 +13,70 @@ const getResult = async (query) => {
   }
 };
 
-const displayResult = (result, contenuto) => {
-  const songs = result.data;
-  for (let i = 0; i < 1; i++) {
-    const song = songs[i];
-    console.log(song);
-    contenuto.innerHTML += /*html*/ `
-        <div class="card col-md-2 p-3 align-items-center" id="${song.id}">
-        <img src="${song.artist.picture_small}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <a class="artistPage" href="../artist/artist.html?id=${song.artist.id}"><h6 class="card-text">${song.artist.name}</h6></a>
-          <p class="card-text">${song.artist.type}</p>
+// creazione card in base alla ricerca:
+
+// card artista
+const createArtistCard = (artist) => {
+  return /*html*/ `
+      <div class="card col-md-2 p-3 align-items-center" id="artist-card">
+        <img src="${artist.picture_medium}" class="card-img-top rounded-circle" alt="${artist.name}">
+        <div class="card-body text-center">
+            <a href="../artist/artist.html?id=${artist.id}"><h6 class="card-title">${artist.name}</h6></a>
+            <p class="card-text">${artist.type}</p>
         </div>
       </div>
-        `;
-  }
+    `;
 };
 
-const searchArtist = async () => {
+// card canzoni
+const createSongCard = (song) => {
+  return /*html*/ `
+      <div class="card col-md-2 p-3 align-items-center" id="song-card">
+        <img src="${song.album.cover_medium}" class="card-img-top rounded-circle" alt="${song.title}">
+        <div class="card-body text-center">
+            <h1 class="card-title">${song.title}</h1>
+            <p class="card-text">${song.type} · <a href="../artist/artist.html?id=${artist.id}">${song.artist.name}</p>
+        </div>
+      </div>
+    `;
+};
+
+// card album
+const createAlbumCard = (album) => {
+    const releaseYear = album.release_date ? album.release_date.split("-")[0] : "N/A";
+    return /*html*/ `
+      <div class="card col-md-2 p-3 align-items-center" id="album-card">
+        <img src="${album.cover_medium}" class="card-img-top rounded-circle" alt="${album.title}">
+        <div class="card-body text-center">
+            <a href="../album/album.html?id=${album.id}"><h1 class="card-title">${album.title}</h1></a>
+            <p class="card-text">${releaseYear} · <a href="../artist/artist.html?id=${artist.id}">${album.artist.name}</a></p>
+        </div>
+      </div>
+    `;
+}
+
+const displayResult = (result, contenuto) => {
+  const songs = result.data;
+  contenuto.innerHTML = "";
+
+  songs.forEach((song) => {
+    if (item.type === "artist") {
+      contenuto.innerHTML += createArtistCard(item.artist);
+    } else if (item.type === "track") {
+      contenuto.innerHTML += createSongCard(item);
+    } else if (item.type === "album") {
+      contenuto.innerHTML += createAlbumCard(item.album);
+    }
+  });
+};
+
+const searchContent = async () => {
   const query = searchInput.value.trim();
   if (query.length > 0) {
     const result = await getResult(query);
     contenuto.innerHTML = "";
     displayResult(result, contenuto);
   } else {
-    contenuto.innerHTML = "";
-  }
-};
-
-const fetchAlbums = async () => {
-  try {
-    const response = await fetch(
-      "https://striveschool-api.herokuapp.com/api/deezer/album/"
-    );
-    if (!response.ok) {
-      throw new Error("Errore nella richiesta");
-    }
-    const data = await response.json();
-    if (!Array.isArray(data.albums)) {
-      throw new Error("Il dato non è un array");
-    }
-
-    data.albums.forEach((item) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
-      const cardContent = `
-            <img src="${item.cover_medium}" class="card-img-top" alt="${item.title}">
-                <div class="card-body">
-                    <h5 class="card-text">${item.title}</h5>
-                    <span class="fs-6">${item.artist.name}</span>
-                </div>
-            `;
-        card.innerHTML = cardContent;
-        cardContent.appendChild(card)
-    });
-  } catch (error) {
-    console.error("Errore nella chiamata API", error);
+    contenuto.innerHTML = "<h1> Inserisci un termine di ricerca </h1>";
   }
 };
